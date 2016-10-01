7e393422bfba48ef2a1d1bb42c7b2237bf7dc2fe
Jean-Philippe Quéméner <jeanphilippe.quemener@gmail.com>
2016-10-01 11:28:06 +0200

    import hammerjs

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -2,7 +2,7 @@
  * Bootstraps the application and makes the ROUTER_PROVIDERS and the APP_BASE_HREF available to it.
  * @see https://angular.io/docs/ts/latest/api/platform-browser-dynamic/index/bootstrap-function.html
  */
-/// <reference path="../../../typings/globals/hammerjs/index.d.ts" />
+import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 // The browser platform with a compiler
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
