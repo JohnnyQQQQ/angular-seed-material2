9ec8580d7e447f5ea4266d2bf6370b6a117e67d2
Jean-Philippe Quemener <jeanphilippe.quemener@gmail.com>
2018-03-11 20:37:18 +0100

    fix: add shared module to core module

--- src/client/app/core/core.module.ts
+++ src/client/app/core/core.module.ts
@@ -4,12 +4,12 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { throwIfAlreadyLoaded } from './module-import-guard';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule, SharedModule],
   declarations: [NavbarComponent, ToolbarComponent],
-  exports: [RouterModule,
-    NavbarComponent, ToolbarComponent]
+  exports: [RouterModule, NavbarComponent, ToolbarComponent]
 })
 export class CoreModule {
 
